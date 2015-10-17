bcf209d8da140f4ff498cda2571a2b52b5bc62f7
Brynjar Þór Björnsson <brynjarthor@i-7f3b5dd9.csd07cloud.internal>
2015-10-17 16:41:41 +0000

    added testEmptySTring, testOnenumber, testTwoNumber, testMultipleNumber

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -11,20 +11,19 @@ public class CalculatorTest {
 
 	@Test
 	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
+		assertEquals(0,Calculator.add(""));
 	}
-
 	@Test
 	public void testOneNumber(){
 		assertEquals(1, Calculator.add("1"));
 	}
 	@Test
-	public void testTwoNumbers(){
-		assertEquals(3, Calculator.add("1,2"));
+	public void testTwoNumber(){
+		assertEquals(4, Calculator.add("1,3"));
+	}
+	@Test 
+	public void testMultipleNumber(){
+		assertEquals(6,Calculator.add("2,3,1"));
 	}
 
-	@Test
-	public void testMultipleNumbers(){
-		assertEquals(6, Calculator.add("1,2,3"));
-	}
 }
